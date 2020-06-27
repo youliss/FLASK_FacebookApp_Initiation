bc13a6f0d79adee3d641febc2ade574b4c5f207e
Youliss <ulysse.le@gmail.com>
2020-06-27 12:57:31 +0200

    miseenproduction2

--- config.py
+++ config.py
@@ -1,8 +1,15 @@
 import os
 
 SECRET_KEY = "#d#JCqTTW\nilK\\7m\x0bp#\tj~#H"
-FB_APP_ID = 259619528523697
 
+
+if os.environ.get('DATABASE_URL') is None:
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+    FB_APP_ID = 626175137996713
+else:
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    FB_APP_ID = 2875937649201270
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
